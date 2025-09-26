03a8173fdc919610546d8a661ccded25c950c3e7
IT22332158 <154005612+GeshikaThennakoon@users.noreply.github.com>
2025-09-26 12:51:01 +0530

    Changes done

--- EV_ChargingStationBooking_system-EAD.Api/obj/Debug/net9.0/EV_ChargingStationBooking_system-EAD.Api.AssemblyInfo.cs
+++ EV_ChargingStationBooking_system-EAD.Api/obj/Debug/net9.0/EV_ChargingStationBooking_system-EAD.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EV_ChargingStationBooking_system-EAD.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6fe8b0f93b87f663888be3ed63412580af22789")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40df69740c631ff4c400825988d769d3978da0c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EV_ChargingStationBooking_system-EAD.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EV_ChargingStationBooking_system-EAD.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
