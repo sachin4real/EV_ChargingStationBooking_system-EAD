9bda1998d871ad9e66219feb3f18b6a3b24a54e6
Bhanuka Fernando <bhanuka.fernando@arimaclanka.com>
2025-10-10 07:57:54 +0530

    up

--- EV_ChargingStationBooking_system-EAD.Api/obj/Debug/net9.0/EV_ChargingStationBooking_system-EAD.Api.AssemblyInfo.cs
+++ EV_ChargingStationBooking_system-EAD.Api/obj/Debug/net9.0/EV_ChargingStationBooking_system-EAD.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EV_ChargingStationBooking_system-EAD.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75dc4213bdaad97cb673515e944860924b325ba8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82d456a4f6110ace2aef8b3f3af836cdc8c7b0e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("EV_ChargingStationBooking_system-EAD.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EV_ChargingStationBooking_system-EAD.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
